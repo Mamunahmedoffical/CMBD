83a831d90df5e9d44c6ae0d1e561400a20486dd4
Mamun Ahmed <psmfarhan@gmail.com>
2024-06-10 21:12:39 +0600

    cmbd all file

---
+++ Java/basic.js
@@ -0,0 +1,20 @@
+var number1 = 40;
+var number2 = 50;
+var myName = "Mamun";
+var userName = "Javascript";
+//   console.log(myName);
+//   console.log(number1);
+//   console.log(number2);
+
+var numOne = 10;
+var numTwo = 2;
+var addition = numOne + numTwo;
+var subtraction = numOne - numTwo;
+var multiplication = numOne * numTwo;
+var division = numOne / numTwo;
+
+console.log("addition:", addition);
+console.log("subtraction:", subtraction);
+console.log("multiplication:", multiplication);
+console.log("division:", division);
+console.log(typeof userName);
